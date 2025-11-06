b5e9a2ae955ca03d1ea80f5af5b93c4d51e7e33f
MaribelMOA <maribel.mendez@uabc.edu.mx>
2025-11-05 19:16:34 -0800

    Initial clinit model and migrations

---
+++ database/seeders/ClinicsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClinicsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
