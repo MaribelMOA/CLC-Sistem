238157985bffda2a20d690c8a3cf638cef423a69
Erick Cano <Erick.Eduardo.Cano.Osorio@uabc.edu.mx>
2025-11-19 19:34:37 -0800

    fix test

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Illuminate\Support\Facades\Hash;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 class User extends Authenticatable
 {
+    use HasFactory;
     use Notifiable;
+    use TwoFactorAuthenticatable;
 
-    protected $fillable = ['name','email','password','role','is_active','specialty','license_number'];
+    protected $fillable = ['name','email','password','role','is_active','specialty','license_number','email_verified_at','two_factor_secret','two_factor_recovery_codes','two_factor_confirmed_at',];
 
-    protected $hidden = ['password','remember_token'];
+    protected $hidden = ['password','remember_token','two_factor_secret','two_factor_recovery_codes',];
+
+    //encriptar con bcrypt
+    public function setPasswordAttribute($value){
+        if (! empty($value)) {
+            $this->attributes['password'] = Hash::needsRehash($value)
+                ? Hash::make($value)
+                : $value;
+        }
+    }
 
     public function clinics(){
         return $this->belongsToMany(Clinic::class);
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration {
             $table->boolean('is_active')->default(true);
             $table->string('specialty')->nullable();
             $table->string('license_number')->nullable()->index();
+
+            // PARA PRUEBAS NO FALLEN
+            $table->timestamp('email_verified_at')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
